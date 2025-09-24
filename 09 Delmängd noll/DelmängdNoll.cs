6e777fba4f07809453b1b6e13e0210c91b94eb43
Claes Engelin <claes.engelin@zocom.se>
2025-09-24 11:51:41 +0200

    Efter uppgift 10

--- 09 Delmängd noll/DelmängdNoll.cs
+++ 09 Delmängd noll/DelmängdNoll.cs
@@ -5,6 +5,35 @@
  *      Exempel: 3, -2, 1, 1, 8 -> 1+1-2=0
 */
 
+int[] numbers = { 3, -2, 1, 1, 8 };
+long subsets = (long)(Math.Pow(2, numbers.Length) - 1);
+
+for (int i = 1; i <= subsets; i++)
+{
+    List<int> currentSum = new List<int>();
+
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        if ((i >> j & 1) == 1)
+        {
+            currentSum.Add(numbers[j]);
+        }
+
+    }
+
+    if (currentSum.Sum() == 0)
+    {
+        Console.Write("Summa noll: ");
+        for (int j = 0; j < currentSum.Count; j++)
+        {
+            Console.Write(j > 0 ?
+                (currentSum[j] > 0 ? " + " + currentSum[j] + "" : " - " + Math.Abs(currentSum[j]) + "")
+                : currentSum[j].ToString());
+        }
+        Console.WriteLine(" = 0\n");
+        break;
+    }
+}
 
 
 
--- 10 Bonuspoäng/Bonuspoäng.cs
+++ 10 Bonuspoäng/Bonuspoäng.cs
@@ -9,8 +9,38 @@
  *      Använd en switch och avsluta med att skriva ut det nya värdet på konsolen
  */
 
+short digit;
+Console.Write("Ange en siffra [1;9]: ");
+bool isDigit = short.TryParse(Console.ReadLine(), out digit);
 
+if (isDigit == false || digit > 9 || digit <= 0)
+{
+    Console.WriteLine("\nFel! Indata var inte i det tillåtna intervallet [1;9]...\n");
+    return;
+}
 
+switch (digit)
+{
+    case 1:
+    case 2:
+    case 3:
+        digit = (short)(digit * 10);
+        break;
+
+    case 4:
+    case 5:
+    case 6:
+        digit = (short)(digit * 100);
+        break;
+
+    case 7:
+    case 8:
+    case 9:
+        digit = (short)(digit * 1000);
+        break;
+}
+
+Console.WriteLine("\nNytt värde: {0}\n", digit);
 
 
 Console.Write("\n\nTryck på en tangent för att fortsätta...");
