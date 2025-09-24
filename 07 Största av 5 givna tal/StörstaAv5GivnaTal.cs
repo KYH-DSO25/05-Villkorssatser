8c6a4e21737e43ebdb30e9a195767001a9362648
Claes Engelin <claes.engelin@zocom.se>
2025-09-24 11:01:53 +0200

    Efter uppggift 8

--- 07 Största av 5 givna tal/StörstaAv5GivnaTal.cs
+++ 07 Största av 5 givna tal/StörstaAv5GivnaTal.cs
@@ -3,6 +3,31 @@
  */
 
 
+decimal[] numbers = { -1, 5, -4, 3, 2 };
+decimal max = numbers[0];
+
+//Console.Write("Tal: {0}", numbers[0] + "  ");
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//    Console.Write(numbers[i] + "  ");
+
+//    if (numbers[i] > max)
+//    {
+//        max = numbers[i];
+//    }
+//}
+
+Console.Write("Tal: ");
+foreach (var number in numbers)
+{
+    Console.Write(number + "  ");
+
+    if (number > max)
+    {
+        max = number;
+    }
+}
+Console.WriteLine("\n\nStörsta talet: {0}\n", max);
 
 
 
--- 08 Kolla typ för indata/KollaTypFörIndata.cs
+++ 08 Kolla typ för indata/KollaTypFörIndata.cs
@@ -6,6 +6,38 @@
  *      Programmet skall visa variabelns värde på konsolen. Använd en switch.
 */
 
+double number = 0;
+string text = string.Empty;
+
+Console.WriteLine("Ange datatyp:");
+Console.WriteLine("    1: int");
+Console.WriteLine("    2: double");
+Console.WriteLine("    3: string");
+Console.Write("-> Ange ditt val: ");
+
+switch (Console.ReadLine()) // istället för hjälpvariabel. Kanske inte världens bästra idé, men det funkar
+{
+    case "1":
+        Console.Write("\nAnge ett heltal: ");
+        number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nytt värde -> {0}\n", number + 1);
+        break;
+    case "2":
+        Console.Write("\nAnge ett flyttal: ");
+        number = double.Parse(Console.ReadLine());
+        Console.WriteLine("Nytt värde -> {0}\n", number + 1);
+        break;
+    case "3":
+        Console.Write("\nAnge en text: ");
+        text = Console.ReadLine();
+        Console.WriteLine("Nytt värde -> {0}*\n", text);
+        break;
+
+
+    default:
+        Console.WriteLine("\nFel! Felaktigt menyval!\n");
+        break;
+}
 
 
 
